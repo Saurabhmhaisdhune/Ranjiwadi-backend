7cd3fc785cd3491d3f189064d719f82689fab197
saurabh <saurabhmhaisdhune1@gmail.com>
2023-04-14 16:54:39 +0530

    commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = 4550;
 
 // const MONGO_URL = "mongodb://127.0.0.1";
 const MONGO_URL = process.env.MONGO_URL;
